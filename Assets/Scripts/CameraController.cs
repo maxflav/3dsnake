f304a505bf22cfc31ef327f1882966f1da1e67ab
Max Friedman <maxcfriedman@gmail.com>
2016-03-01 10:21:11 -0800

    Add Unity project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject head;
+
+	// private Vector3 offset;
+
+	void Start ()
+	{
+		// offset = transform.position - head.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		// transform.position = head.transform.position + offset;
+		// transform.rotation = head.transform.rotation;
+	}
+}
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeController : MonoBehaviour {
+	public GameObject head;
+
+	private Vector3 direction = new Vector3 (0, 0, 1);
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = Quaternion.LookRotation (direction);
+		StartCoroutine (MoveCycle ());
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("w")) {
+			//Debug.Log (direction);
+			direction = Quaternion.AngleAxis (90, Vector3.left) * direction;
+			//Debug.Log (direction);
+		}
+		else if (Input.GetKeyDown ("a")) {
+			//Debug.Log (direction);
+			direction = Quaternion.AngleAxis (90, Vector3.forward) * direction;
+			//Debug.Log (direction);
+		}
+		else if (Input.GetKeyDown ("s")) {
+			//Debug.Log (direction);
+			direction = Quaternion.AngleAxis (90, Vector3.right) * direction;
+			//Debug.Log (direction);
+		}
+		else if (Input.GetKeyDown ("d")) {
+			Debug.Log (direction);
+			direction = Quaternion.AngleAxis (90, Vector3.up) * direction;
+			Debug.Log (direction);
+		}
+		
+		transform.localRotation = Quaternion.LookRotation (direction);
+	}
+
+	IEnumerator MoveCycle() {
+		Debug.Log ("!!!! MoveCycle() !!!!");
+		while (true) {
+			Move ();
+			yield return new WaitForSeconds (1);
+		}
+	}
+
+	void Move() {
+		print ("********* MOVE() *********");
+		print ("old head position: " + head.transform.position);
+		GameObject newPart = (GameObject)Instantiate (head, head.transform.position, head.transform.rotation);
+		print ("bodypart position: " + newPart.transform.position);
+		newPart.GetComponent<Renderer>().material.color = Color.green;
+		print ("Translating head:" + direction);
+		head.transform.Translate (direction);
+		print("new head position: " + head.transform.position);
+	}
+}
