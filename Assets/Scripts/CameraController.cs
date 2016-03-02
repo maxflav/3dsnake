afce30108befc635a23a869a1d5e26c099e1c1f1
Max Friedman <maxcfriedman@gmail.com>
2016-03-01 21:31:02 -0800

    Dying + Smooth camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,8 +5,44 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject head;
 
-	void LateUpdate () {
+	private Vector3 lastCameraPosition;
+	private Vector3 currentHeadPosition;
+	private float lastPositionTime;
+
+	private Quaternion lastCameraRotation;
+	private Quaternion currentHeadRotation;
+	private float lastRotationTime;
+
+	void Start () {
 		transform.position = head.transform.position;
+		lastCameraPosition = head.transform.position;
+		currentHeadPosition = head.transform.position;
+		lastPositionTime = Time.time;
+
 		transform.rotation = head.transform.rotation;
+		lastCameraRotation = head.transform.rotation;
+		currentHeadRotation = head.transform.rotation;
+		lastRotationTime = Time.time;
+	}
+
+	void Update () {
+
+		transform.position = Vector3.Lerp (lastCameraPosition, currentHeadPosition, (Time.time - lastPositionTime));
+
+		if (head.transform.position != currentHeadPosition) {
+			// Head has moved
+			lastCameraPosition = transform.position;
+			currentHeadPosition = head.transform.position;
+			lastPositionTime = Time.time;
+		}
+
+		transform.rotation = Quaternion.Lerp (lastCameraRotation, currentHeadRotation, (Time.time - lastRotationTime) * 3);
+
+		if (head.transform.rotation != currentHeadRotation) {
+			// Head has turned
+			lastCameraRotation = transform.rotation;
+			currentHeadRotation = head.transform.rotation;
+			lastRotationTime = Time.time;
+		}
 	}
 }
--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SnakeController : MonoBehaviour {
 	public GameObject head;
+	private Queue<GameObject> tailQ = new Queue<GameObject> ();
+
+	private int globalPitch = 0;
+	private int globalYaw = 0;
+
+	private GameObject neck;
 
 	void Start () {
+		head.GetComponent<MeshRenderer> ().enabled = false;
 		StartCoroutine (MoveCycle ());
 	}
 
 	void Update () {
-		float pitch = 0;
-		float yaw = 0;
-		float roll = 0;
+		int pitch = 0;
+		int yaw = 0;
+		int roll = 0;
 
 		if (Input.GetKeyDown ("w")) { pitch += 1; }
 		if (Input.GetKeyDown ("s")) { pitch -= 1; }
@@ -26,6 +34,16 @@ public class SnakeController : MonoBehaviour {
 			return;
 		}
 
+		// These prevent you from spinning all the way round.
+		// The head can't do a full 180 otherwise it would immediately die.
+		// Twisting around with Roll is fine I guess.
+		// Actually this doesn't completely work because if yaw=1 and roll=1 and pitch=1 you end up spun around.
+		// Shrug oh well lol
+		if (globalPitch + pitch >= 2) { pitch = 0; }
+		if (globalYaw + yaw >= 2) { yaw = 0; }
+		globalPitch += pitch;
+		globalYaw += yaw;
+
 		Vector3 rotVector = new Vector3(-pitch, yaw, -roll);
 		head.transform.Rotate(rotVector * 90);
 	}
@@ -38,8 +56,44 @@ public class SnakeController : MonoBehaviour {
 	}
 
 	void Move() {
-		GameObject newPart = (GameObject)Instantiate (head, head.transform.position, head.transform.rotation);
-		newPart.GetComponent<Renderer>().material.color = Color.green;
+		if (neck != null) {
+			// Tail part immediately behind head (and head) are not visible, otherwise they overlap camera too much
+			neck.GetComponent<MeshRenderer> ().enabled = true;
+		}
+
+		neck = (GameObject)Instantiate (head, head.transform.position, head.transform.rotation);
+		neck.GetComponent<MeshRenderer> ().enabled = false;
+		neck.GetComponent<Renderer>().material.color = Color.green;
+		tailQ.Enqueue (neck);
+
+		globalPitch = 0;
+		globalYaw = 0;
 		head.transform.Translate (Vector3.forward);
+
+		if (Mathf.Abs(head.transform.position.x) > 5 || Mathf.Abs(head.transform.position.y) > 5 || Mathf.Abs(head.transform.position.z) > 5) {
+			head.transform.Translate (Vector3.back);
+			die ();
+			return;
+		}
+
+		foreach (GameObject tailPart in tailQ) {
+			if (samePosition(head.transform.position, tailPart.transform.position)) {
+				head.transform.Translate (Vector3.back);
+				die ();
+				return;
+			}
+		}
+	}
+
+	bool samePosition(Vector3 a, Vector3 b) {
+		return closeFloats (a.x, b.x) && closeFloats (a.y, b.y) && closeFloats (a.z, b.z);
+	}
+
+	bool closeFloats(float a, float b) {
+		return Mathf.RoundToInt (a) == Mathf.RoundToInt (b);
+	}
+
+	void die() {
+		Debug.Log ("Dead " + head.transform.position);
 	}
 }
